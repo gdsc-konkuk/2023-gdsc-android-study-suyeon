5db661269cbb54970e77edb818d9ee39c3439340
suyeun84 <kelsey6225@naver.com>
2023-10-30 14:20:43 +0900

    [#1/Feat] 마이페이지 구현

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,11 @@ plugins {
 
 android {
     namespace = "kr.ac.konkuk.gdsc.gdscsuyeon"
-    compileSdk = 33
+    compileSdk = 34
+
+    buildFeatures {
+        viewBinding = true
+    }
 
     defaultConfig {
         applicationId = "kr.ac.konkuk.gdsc.gdscsuyeon"
@@ -37,7 +41,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
