324e7dcca402099b591a801f49a39eb310b822ad
suyeun84 <kelsey6225@naver.com>
2023-11-03 00:11:02 +0900

    [#1/Mod] ViewModel/DataBinding

--- app/src/main/java/kr/ac/konkuk/gdsc/gdscsuyeon/EditActivity.kt
+++ app/src/main/java/kr/ac/konkuk/gdsc/gdscsuyeon/EditActivity.kt
@@ -1,29 +1,76 @@
 package kr.ac.konkuk.gdsc.gdscsuyeon
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.internal.ViewUtils
 import kr.ac.konkuk.gdsc.gdscsuyeon.databinding.ActivityEditBinding
 
 class EditActivity : AppCompatActivity() {
     private lateinit var binding: ActivityEditBinding
+    private lateinit var myEditViewModel: EditViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEditBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        myEditViewModel = ViewModelProvider(this).get(EditViewModel::class.java)
+
+        binding.editVM = myEditViewModel
+        binding.lifecycleOwner = this
+
+        initNickname()
         binding.ivBackButton.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
+            navigateToMainActivity(myEditViewModel.currentName.value.toString())
         }
+        updateNickname()
+        toHideKeyboard()
+    }
+    private fun initNickname() {
+        val myname = intent.getStringExtra("nickname")
+        if (myname != null) {
+            myEditViewModel.updateValue(ActionType.START, myname.toString())
+        }
+    }
+
+    private fun navigateToMainActivity(nickname: String) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.putExtra("nickname", nickname)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun updateNickname() {
         binding.storage.setOnClickListener {
-            if (binding.editNickname.length() == 0) {
-                Toast.makeText(this, "닉네임은 빈칸일 수 없습니다", Toast.LENGTH_SHORT).show()
-            }else{
-                binding.myName.text = binding.editNickname.text
+            val userInput = binding.editNickname.text
+            if (userInput.isNotBlank()) {
+                myEditViewModel.updateValue(ActionType.EDIT, userInput.toString())
                 binding.editNickname.text.clear()
+            } else {
+                showToast("입력값이 없습니다.")
             }
+        }
+        //LiveData 사용하여 데이터 변경 감지 및 UI 업데이트
+        myEditViewModel.currentName.observe(this) { name ->
+            binding.myName.text = name
+        }
+    }
+
+    private fun showToast(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
+
+    @SuppressLint("RestrictedApi")
+    private fun toHideKeyboard() {
+        binding.root.setOnClickListener {
+            ViewUtils.hideKeyboard(currentFocus ?: View(this))
+            binding.editNickname.clearFocus()
         }
     }
 }
--- app/src/main/java/kr/ac/konkuk/gdsc/gdscsuyeon/MainActivity.kt
+++ app/src/main/java/kr/ac/konkuk/gdsc/gdscsuyeon/MainActivity.kt
@@ -2,18 +2,31 @@ package kr.ac.konkuk.gdsc.gdscsuyeon
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kr.ac.konkuk.gdsc.gdscsuyeon.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.myInfoChangeBtn.setOnClickListener {
-            startActivity(Intent(this, EditActivity::class.java))
+        val myname = intent.getStringExtra("nickname")
+        if(myname != null) {
+            binding.myName.text = myname.toString()
         }
+
+        binding.myInfoChangeBtn.setOnClickListener {
+            navigateToEditActivity(binding.myName.text.toString())
+        }
+    }
+
+    private fun navigateToEditActivity(nickname: String) {
+        val intent = Intent(this, EditActivity::class.java)
+        intent.putExtra("nickname", nickname)
+        startActivity(intent)
     }
 }
