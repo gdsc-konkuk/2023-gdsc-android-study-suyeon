f14a49c1c2a21ba25d9ec45a20faab0d1b764362
suyeun84 <kelsey6225@naver.com>
2023-11-27 07:01:40 +0900

    [#10/Feat] 완료 투두 개수 myPage에 표시

--- app/src/main/java/kr/ac/konkuk/gdsc/gdscsuyeon/ui/mypage/MyPageFragment.kt
+++ app/src/main/java/kr/ac/konkuk/gdsc/gdscsuyeon/ui/mypage/MyPageFragment.kt
@@ -1,25 +1,37 @@
 package kr.ac.konkuk.gdsc.gdscsuyeon.ui.mypage
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context.MODE_PRIVATE
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import kr.ac.konkuk.gdsc.gdscsuyeon.R
 import kr.ac.konkuk.gdsc.gdscsuyeon.databinding.FragmentMyPageBinding
 import kr.ac.konkuk.gdsc.gdscsuyeon.ui.edit.EditActivity
+import kr.ac.konkuk.gdsc.gdscsuyeon.ui.home.HomeViewModel
 
+@AndroidEntryPoint
 class MyPageFragment : Fragment() {
     private lateinit var sharedPreferences: SharedPreferences
     private lateinit var editor: SharedPreferences.Editor
     private lateinit var userName: String
-    private var _binding: FragmentMyPageBinding?= null
+    private var _binding: FragmentMyPageBinding? = null
     private val binding
-        get() = requireNotNull(_binding) {"MyPageFragment binding is null"}
+        get() = requireNotNull(_binding) { "MyPageFragment binding is null" }
+    private val viewModel by viewModels<MyPageViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -30,8 +42,15 @@ class MyPageFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentMyPageBinding.inflate(layoutInflater, container, false)
+
+        lifecycleScope.launch {
+            viewModel.loadDoneTodoNum() // 비동기 호출
+            // 로딩이 완료된 후에 값을 가져와서 사용
+            binding.completedTodo.text =
+                getString(R.string.completed_todo_count, viewModel.doneTodoNum.value)
+        }
 
         val nickname = sharedPreferences.getString("mypagename", "fail")
         if (nickname != null) {
